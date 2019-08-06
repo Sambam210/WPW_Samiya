1e868d96f9b8e8eab3bbfa0c7eb5a023cbd33faa
Sambam210 <samiya.tabassum@mq.edu.au>
2019-08-06 13:50:43 +1000

    Started a new script for MFA

---
+++ scripts/Multiple_Factor_Analysis.R
@@ -0,0 +1,10 @@
+# Following a tutorial for MFA
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/116-mfa-multiple-factor-analysis-in-r-essentials/
+
+install.packages(c("FactoMineR", "factoextra"))
+
+library("FactoMineR")
+library("factoextra")
+
+# using the wine dataset in FactoMineR
+
