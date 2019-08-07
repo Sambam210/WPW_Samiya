83a580091724c51de2ccd2a4beacb75d25e94795
Sambam210 <samiya.tabassum@mq.edu.au>
2019-08-07 13:34:50 +1000

    completed the practise PCA

--- scripts/Multiple_Factor_Analysis.R
+++ scripts/Multiple_Factor_Analysis.R
@@ -71,6 +71,7 @@ quanti.var
 # also check out all the other types of multivariate analyses
 # http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/
 
+
 #######################################################################################################################################################################
 ############################################################ Practice PCA analysis ####################################################################################
 #######################################################################################################################################################################
@@ -90,6 +91,168 @@ library(tidyverse)
 practise_PCA <- practise_PCA %>%
   remove_rownames %>%
   column_to_rownames(var="Species_Code")
+
+library("FactoMineR")
+library("factoextra")
+
+# subset the data to remove the categorical variables, these will be used later to colour by groups
+
+practise_PCA_cont_var <- select(practise_PCA, -Origin, -Growth_Form)
+
+## scaling the data
+# data need to be in the same scale before analysis
+# PCA() in FActoMineR does this automatically
+
+# doing the PCA
+
+res.pca <- PCA(practise_PCA_cont_var, graph = FALSE)
+
+print(res.pca)
+
+# looking at the eigenvalues
+
+eig.val <- get_eigenvalue(res.pca)
+eig.val
+# first three PCs explain 92% of the data
+
+# let's look at the scree plot
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+
+# let's look at the variables
+
+var <- get_pca_var(res.pca)
+var
+
+# plot the variables
+
+fviz_pca_var(res.pca, col.var = "black")
+
+# quality of representation
+
+install.packages("corrplot")
+library("corrplot")
+corrplot(var$cos2, is.corr=FALSE)
+# think this means that OsmPot is correlated with Dim2 while succulance, thickness and LMA is correlated with Dim1
+
+# colour variables on the correlation circle by their cos2 value -> quality of the variable on the factor map
+
+fviz_pca_var(res.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), # specify custom gradient
+             repel = TRUE # avoid text overlap
+             )
+
+# change the transparecny of variables according to the cos2 value
+
+fviz_pca_var(res.pca, alpha.var = "cos2")
+
+# contributions of variables to PCs
+
+corrplot(var$contrib, is.corr=FALSE)
+
+# Contributions of variables to PC1
+
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 4) # red dashed line is the expected average contribution (i.e. 4 variables = 25%)
+
+# Contributions of variables to PC2
+
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 4)
+
+# Contributions of variables to PC3
+
+fviz_contrib(res.pca, choice = "var", axes = 3, top = 4)
+
+# dimension description
+# ID the most significanlty associated variables for a given PC
+
+res.desc <- dimdesc(res.pca, axes = c(1,2), proba = 0.05)
+# Description of dimension 1
+res.desc$Dim.1
+# Description of dimension 2
+res.desc$Dim.2
+
+# graph of individuals
+
+ind <- get_pca_ind(res.pca)
+ind
+
+# plot the individuals
+
+fviz_pca_ind(res.pca, repel = TRUE)
+
+# colour individuals bt their cos2 value
+
+fviz_pca_ind(res.pca, col.ind = "cos2", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE # Avoid text overlapping (slow if many points)
+)
+
+# colour individuals by a custom continuous variable
+# let's try mean aridity index
+
+og_data <- read.csv("Data_output/SamiyaandReneeandHugh.csv")
+# extract the AI_mean column
+AI_mean <- select(og_data, AI_mean)
+# colour individuals by mean AI
+fviz_pca_ind(res.pca, col.ind = AI_mean,
+             gradient.cols = c("blue", "yellow", "red"),
+             legend.title = "AI_mean") # DOESN'T WORK!!!!
+
+# try the example
+
+# Create a random continuous variable of length 23,
+# Same length as the number of active individuals in the PCA
+set.seed(123)
+my.cont.var <- rnorm(113)
+# Color individuals by the continuous variable
+fviz_pca_ind(res.pca, col.ind = my.cont.var,
+             gradient.cols = c("blue", "yellow", "red"),
+             legend.title = "Cont.Var") # WORKS - I THINK IT'S BECASUE THERE ARE NAS IN THE DATASET FOR AI_MEAN
+
+# colour by groups
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = practise_PCA$Growth_Form, # color by growth form
+             palette = c("Dark2"),
+             addEllipses = TRUE, # Concentration ellipses
+             legend.title = "Growth form"
+)
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = practise_PCA$Origin, # color by origin
+             palette = c("Dark2"),
+             addEllipses = TRUE, # Concentration ellipses
+             legend.title = "Origin"
+)
+
+# confidence ellipses (prob better)
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = practise_PCA$Growth_Form, # color by growth form
+             palette = c("Dark2"),
+             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipses
+             legend.title = "Growth form"
+) # grasses look sign different from everything else
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = practise_PCA$Origin, # color by origin
+             palette = c("Dark2"),
+             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipses
+             legend.title = "Origin"
+) # doesn't look different
+
+# biplot
+
+fviz_pca_biplot(res.pca, 
+                col.ind = practise_PCA$Growth_Form, palette = "jco", 
+                addEllipses = TRUE, ellipse.type = "confidence", label = "var",
+                col.var = "black", repel = TRUE,
+                legend.title = "Growth Form")
+
 
 
 
