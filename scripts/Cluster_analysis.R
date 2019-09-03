1b5fc3c6928fd11e48cc1554cc165ea25d914be3
Sambam210 <samiya.tabassum@mq.edu.au>
2019-09-03 11:55:45 +1000

    did first cluster analysis

--- scripts/Cluster_analysis.R
+++ scripts/Cluster_analysis.R
@@ -6,8 +6,18 @@
 
 # try this first
 # cluser analysis after 
-# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/117-hcpc-hierarchical-clustering-on-principal-components-essentials/
 
+############################################################################################################################################################################
+################################################## Hierarchical CLustering after FAMD analysis #############################################################################
+############################################################################################################################################################################
+
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/117-hcpc-hierarchical-clustering-on-principal-components-essentials/
+# http://www.sthda.com/english/wiki/print.php?id=245
+# https://www.youtube.com/watch?v=4XrgWmN9erg&list=PLnZgp6epRBbTsZEFXi_p6W48HhNyqwxIu&index=7
+
+# FAMD reduces the dimensionality f the data which can lead to more stable clustering
+
+# Run the FAMD first
 library(tidyverse)
 
 # Load the data, this includes the glasshouse variables + my scrapped variables (I have modified them to be more informative)
@@ -20,7 +30,7 @@ FAMD_data <- FAMD_data %>%
   remove_rownames %>%
   column_to_rownames(var="Species_Code") # make species code the row names
 
-# remove the categorical vaiables that aren't needed/used to colour points later
+# remove the categorical vaiables that aren't needed/used
 
 FAMD_data_analysis <- select(FAMD_data, -Species, -Origin, -Growth_Form)
 
@@ -31,7 +41,54 @@ str(FAMD_data_analysis)
 
 # running the FAMD analysis
 
-res.famd <- FAMD(FAMD_data_analysis, graph = FALSE)
+res.famd <- FAMD(FAMD_data_analysis, graph = FALSE) # changed the number of principle components to 4 as the 5th conmponent doesn't seem to add much more explanantion. The default ncp for FAMD analysis is 5.
+
+# extract the eigenvalues
+
+eig.val <- get_eigenvalue(res.famd)
+head(eig.val) # first 3 dimensions explain 49% of the data
+
+# draw the scree plot
+fviz_screeplot(res.famd)
+
+## quantitative variables
+
+quanti.var <- get_famd_var(res.famd, "quanti.var")
+
+# plot the quantitative variables
+
+quanti.plot <- fviz_famd_var(res.famd, "quanti.var", repel = TRUE, col.var = "black")
+quanti.plot
+
+## qualitative variables
+
+quali.var <- get_famd_var(res.famd, "quali.var")
+
+# plot the qualitative variables
+
+quali.plot <- fviz_famd_var(res.famd, "quali.var", repel = TRUE, col.var = "black")
+quali.plot
+
+### graph of individuals
+
+ind <- get_famd_ind(res.famd)
+
+ind.graph <- fviz_famd_ind(res.famd, repel = TRUE, invisible = "quali.var", col.ind = "black")
+ind.graph
+
+# colour individuals by growth form (used ?fviz_famd_ind to figure out)
+
+library(ggplot2)
+
+individuals <- fviz_famd_ind(res.famd, 
+                             geom = "point", # show points only (but not "text")
+                             col.ind = FAMD_data$Growth_Form, # color by growth form 
+                             palette = c("Dark2"),
+                             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipse
+                             legend.title = "Growth from",
+                             invisible = "quali.var")
+
+individuals
 
 # Compute hierarchical clustering
 res.hcpc <- HCPC(res.famd, graph = FALSE)
@@ -42,8 +99,7 @@ fviz_dend(res.hcpc,
           palette = "jco",               # Color palette see ?ggpubr::ggpar
           rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
           rect_border = "jco",           # Rectangle color
-          labels_track_height = 0.8      # Augment the room for labels
-)
+          labels_track_height = 0.8)     # Augment the room for labels
 
 # graph
 fviz_cluster(res.hcpc,
@@ -51,6 +107,41 @@ fviz_cluster(res.hcpc,
              show.clust.cent = TRUE, # Show cluster centers
              palette = "jco",         # Color palette see ?ggpubr::ggpar
              ggtheme = theme_minimal(),
-             main = "Factor map"
-)
+             main = "Factor map")
+
+# Let's look at the HCPC output
+
+# display the original data with a new column indicating which cluster they belong to
+
+head(res.hcpc$data.clust)
+
+# display the qualtitative variables that explain the most variance in each cluster
+
+res.hcpc$desc.var$quanti
+
+# same thing but for categorical variables
+res.hcpc$desc.var$category
+
+# principle dimensions that are most associated with clusters
+res.hcpc$desc.axes$quanti
+
+# save the output
+pdf("MFA_data_output/FAMD_scraped_glasshouse.pdf") # Create a new pdf device
+print(quanti.plot)
+print(quali.plot)
+print(individuals)
+dev.off() # Close the pdf device
+
+################################################################################################################################################
+################################################################### Ale's suggestion ####################################################################
+################################################################################################################################################
+
+# 1. PCA and cluster analysis on glasshouse ecophys variables
+# 2. MCA and cluster analysis on the scraped data
+# 3. FAMD and cluster analysis on everything (already did)
+
+# we want to look at how much variation is explained by each analysis and where an additive model is better
+# 'can soft traits be used to predict drought tolerance of plants?'
+
+
 
