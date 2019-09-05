83529661fb96c2da6734d7eab7768c640eb8cd19
Sambam210 <samiya.tabassum@mq.edu.au>
2019-09-05 14:23:42 +1000

    more cluster analysis

--- scripts/Cluster_analysis.R
+++ scripts/Cluster_analysis.R
@@ -1378,6 +1378,7 @@ library("factoextra")
 # need to transform the succulance variable to make it linear
 PCA_analysis <- all.data %>%
   mutate(log_mean_succulance_g = log(mean_succulance_g)) %>%
+  filter(Species_Code != "Phro") %>% # remove Phro because it has NAs for climate variables
   select(-mean_succulance_g) %>%
   remove_rownames %>%
   column_to_rownames(var="Species_Code") # make species code the row names
@@ -1396,7 +1397,7 @@ print(res.pca)
 
 eig.val <- get_eigenvalue(res.pca)
 eig.val
-# first three PCs explain 92% of the data
+# first three PCs explain 77% of the data
 
 # let's look at the scree plot
 
@@ -1409,23 +1410,14 @@ var
 
 # plot the variables
 
-var.plot <- fviz_pca_var(res.pca, col.var = "black")
+var.plot <- fviz_pca_var(res.pca, col.var = "black", repel = TRUE)
 var.plot
-
-# quality of representation
-
-install.packages("corrplot")
-library("corrplot")
-corrplot(var$cos2, is.corr=FALSE)
-# think this means that OsmPot is correlated with Dim2 while succulance, thickness and LMA is correlated with Dim1
 
 # colour variables on the correlation circle by their contrib value -> contribution of the variable on the factor map
 
 fviz_pca_var(res.pca, col.var = "contrib",
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), # specify custom gradient
-             repel = TRUE # avoid text overlap
-)
-
+             repel = TRUE) # avoid text overlap
 
 # Contributions of variables to PC1
 
@@ -1435,18 +1427,14 @@ fviz_contrib(res.pca, choice = "var", axes = 1, top = 4) # red dashed line is th
 
 fviz_contrib(res.pca, choice = "var", axes = 2, top = 4)
 
-# Contributions of variables to PC3
-
-fviz_contrib(res.pca, choice = "var", axes = 3, top = 4)
-
 # dimension description
 # identify the most significanlty associated variables for a given PC
 
 res.desc <- dimdesc(res.pca, axes = c(1,2), proba = 0.05)
 # Description of dimension 1
-res.desc$Dim.1 # thickness
+res.desc$Dim.1 # LMA, thickness, annual precip
 # Description of dimension 2
-res.desc$Dim.2 # osmotic potential and LMA
+res.desc$Dim.2 # succulence
 
 # graph of individuals
 
@@ -1522,13 +1510,341 @@ res.hcpc$desc.var$quanti
 res.hcpc$desc.axes$quanti
 
 # save the output
-pdf("MFA_data_output/Cluster_analysis/PCA_glasshouse_trees_shrubs.pdf") # Create a new pdf device
+pdf("MFA_data_output/Cluster_analysis/PCA_glasshouse_climate_trees_shrubs.pdf") # Create a new pdf device
 print(var.plot)
 print(biplot)
 print(ind.plot)
 print(cluster)
 dev.off() # Close the pdf device
 
+######################################################################################################################################
+################################### PCA on glasshouse variables and selected environmental variables #################################
+######################################################## plus leaf area ##############################################################
+######################################################################################################################################
+
+other.variables <- read.csv("MFA_data/FAMD_data2.csv")
+
+hugh.data <- read.csv("MFA_data/niche.data.HB.csv")
+
+hugh.data <- hugh.data %>%
+  select(searchTaxon, Annual_precip_mean, Precip_dry_qu_mean) %>% # selecting the relevant variables
+  rename(Species=searchTaxon)
+
+all.data <- left_join(other.variables, hugh.data, by = "Species")
+
+all.data <- filter(all.data, Species_Code != "Phro", Species_Code != "Kebe", Species_Code != "Atmo", Species_Code != "Pich", 
+                     Species_Code != "Ulpa", Species_Code != "Crla", Species_Code != "Crma", Species_Code != "Atfi",
+                     Species_Code != "Brru") # filter out species with missing climate or leaf area
+
+library(tidyverse)
+library("FactoMineR")
+library("factoextra")
+
+# need to transform the succulance variable to make it linear
+PCA_analysis <- all.data %>%
+  mutate(log_mean_succulance_g = log(mean_succulance_g)) %>%
+  select(-mean_succulance_g, -mean_Lobosity) %>% # need to remove lobosity
+  remove_rownames %>%
+  column_to_rownames(var="Species_Code") # make species code the row names
+
+# remove the categorical vaiables
+
+PCA_analysis_cont <- select(PCA_analysis, 9:15)
+
+# doing the PCA
+
+res.pca <- PCA(PCA_analysis_cont, graph = FALSE)
+
+print(res.pca)
+
+# looking at the eigenvalues
+
+eig.val <- get_eigenvalue(res.pca)
+eig.val
+# first 4 PCs explain 86% of the data
+
+# let's look at the scree plot
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+
+# let's look at the variables
+
+var <- get_pca_var(res.pca)
+var
+
+# plot the variables
+
+var.plot <- fviz_pca_var(res.pca, col.var = "black")
+var.plot
+
+# colour variables on the correlation circle by their contrib value -> contribution of the variable on the factor map
+
+fviz_pca_var(res.pca, col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), # specify custom gradient
+             repel = TRUE) # avoid text overlap
+
+# Contributions of variables to PC1
+
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 4) # red dashed line is the expected average contribution (i.e. 4 variables = 25%)
+
+# Contributions of variables to PC2
+
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 4)
+
+# Contributions of variables to PC3
+
+fviz_contrib(res.pca, choice = "var", axes = 3, top = 4)
+
+# dimension description
+# identify the most significanlty associated variables for a given PC
+
+res.desc <- dimdesc(res.pca, axes = c(1,2), proba = 0.05)
+# Description of dimension 1
+res.desc$Dim.1 # succulence and leaf area
+# Description of dimension 2
+res.desc$Dim.2 # LMA and thickness
+
+# graph of individuals
+
+ind <- get_pca_ind(res.pca)
+ind
+
+# plot the individuals
+
+ind.plot <- fviz_pca_ind(res.pca, repel = TRUE)
+ind.plot
+
+# colour by groups
+
+# confidence ellipses
+# shows 95% confidence ellipse, see ?coord.ellipse()
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = PCA_analysis$Growth_Form, # color by growth form
+             palette = c("Dark2"),
+             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipses
+             legend.title = "Growth form") # grasses look sign different from everything else
+
+
+# biplot
+
+biplot <- fviz_pca_biplot(res.pca, 
+                          col.ind = PCA_analysis$Growth_Form, palette = "jco", 
+                          addEllipses = TRUE, ellipse.type = "confidence", label = "var",
+                          col.var = "black", repel = TRUE,
+                          legend.title = "Growth Form")
+
+biplot
+
+# Hierarchical clustering
+
+# let's only do the PCA on the first 3 dimensions
+
+res.pca <- PCA(PCA_analysis_cont, ncp = 3, graph = FALSE)
+
+# clustering
+res.hcpc <- HCPC(res.pca, graph = FALSE)
+
+# dendrogram
+fviz_dend(res.hcpc, 
+          cex = 0.7,                     # Label size
+          palette = "jco",               # Color palette see ?ggpubr::ggpar
+          rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
+          rect_border = "jco",           # Rectangle color
+          labels_track_height = 0.8)     # Augment the room for labels
+
+# graph
+cluster <- fviz_cluster(res.hcpc,
+                        repel = TRUE,            # Avoid label overlapping
+                        show.clust.cent = TRUE, # Show cluster centers
+                        palette = "jco",         # Color palette see ?ggpubr::ggpar
+                        ggtheme = theme_minimal(),
+                        main = "PCA")
+cluster
+
+# Let's look at the HCPC output
+
+# display the original data with a new column indicating which cluster they belong to
+
+head(res.hcpc$data.clust)
+
+# display the qualtitative variables that explain the most variance in each cluster
+
+res.hcpc$desc.var$quanti
+
+# principle dimensions that are most associated with clusters
+res.hcpc$desc.axes$quanti
+
+# save the output
+pdf("MFA_data_output/Cluster_analysis/PCA_glasshouse_climate_leafarea_all.pdf") # Create a new pdf device
+print(var.plot)
+print(biplot)
+print(ind.plot)
+print(cluster)
+dev.off() # Close the pdf device
+
+#################################### subsetting the data for only the trees and shrubs
+
+other.variables <- read.csv("MFA_data/FAMD_data2.csv")
+
+hugh.data <- read.csv("MFA_data/niche.data.HB.csv")
+
+hugh.data <- hugh.data %>%
+  select(searchTaxon, Annual_precip_mean, Precip_dry_qu_mean) %>% # selecting the relevant variables
+  rename(Species=searchTaxon)
+
+all.data <- left_join(other.variables, hugh.data, by = "Species")
+
+all.data <- filter(all.data, Growth_Form == "Tree" | Growth_Form == "Shrub") # 92 native and exotic trees and shrubs
+
+all.data <- filter(all.data, Species_Code != "Atmo", Species_Code != "Atfi", Species_Code != "Brru", Species_Code != "Crma", Species_Code != "Crla", Species_Code != "Pich",
+                   Species_Code != "Phro", Species_Code != "Ulpa")
+
+library(tidyverse)
+library("FactoMineR")
+library("factoextra")
+
+# need to transform the succulance variable to make it linear
+PCA_analysis <- all.data %>%
+  mutate(log_mean_succulance_g = log(mean_succulance_g)) %>%
+  select(-mean_succulance_g, -mean_Lobosity) %>%
+  remove_rownames %>%
+  column_to_rownames(var="Species_Code") # make species code the row names
+
+# remove the categorical vaiables
+
+PCA_analysis_cont <- select(PCA_analysis, 9:15)
+
+# doing the PCA
+
+res.pca <- PCA(PCA_analysis_cont, graph = FALSE)
+
+print(res.pca)
+
+# looking at the eigenvalues
+
+eig.val <- get_eigenvalue(res.pca)
+eig.val
+# first three PCs explain 77% of the data
+
+# let's look at the scree plot
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+
+# let's look at the variables
+
+var <- get_pca_var(res.pca)
+var
+
+# plot the variables
+
+var.plot <- fviz_pca_var(res.pca, col.var = "black", repel = TRUE)
+var.plot
+
+# colour variables on the correlation circle by their contrib value -> contribution of the variable on the factor map
+
+fviz_pca_var(res.pca, col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), # specify custom gradient
+             repel = TRUE) # avoid text overlap
+
+# Contributions of variables to PC1
+
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 4) # red dashed line is the expected average contribution (i.e. 4 variables = 25%)
+
+# Contributions of variables to PC2
+
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 4)
+
+# dimension description
+# identify the most significanlty associated variables for a given PC
+
+res.desc <- dimdesc(res.pca, axes = c(1,2), proba = 0.05)
+# Description of dimension 1
+res.desc$Dim.1 # LMA, thickness, annual precip
+# Description of dimension 2
+res.desc$Dim.2 # succulence
+
+# graph of individuals
+
+ind <- get_pca_ind(res.pca)
+ind
+
+# plot the individuals
+
+ind.plot <- fviz_pca_ind(res.pca, repel = TRUE)
+ind.plot
+
+# colour by groups
+
+# confidence ellipses
+# shows 95% confidence ellipse, see ?coord.ellipse()
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = PCA_analysis$Growth_Form, # color by growth form
+             palette = c("Dark2"),
+             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipses
+             legend.title = "Growth form") # grasses look sign different from everything else
+
+
+# biplot
+
+biplot <- fviz_pca_biplot(res.pca, 
+                          col.ind = PCA_analysis$Growth_Form, palette = "jco", 
+                          addEllipses = TRUE, ellipse.type = "confidence", label = "var",
+                          col.var = "black", repel = TRUE,
+                          legend.title = "Growth Form")
+
+biplot # not really telling us anything
+
+# Hierarchical clustering
+
+# let's only do the PCA on the first 3 dimensions
+
+res.pca <- PCA(PCA_analysis_cont, ncp = 3, graph = FALSE)
+
+# clustering
+res.hcpc <- HCPC(res.pca, graph = FALSE)
+
+# dendrogram
+fviz_dend(res.hcpc, 
+          cex = 0.7,                     # Label size
+          palette = "jco",               # Color palette see ?ggpubr::ggpar
+          rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
+          rect_border = "jco",           # Rectangle color
+          labels_track_height = 0.8)     # Augment the room for labels
+
+# graph
+cluster <- fviz_cluster(res.hcpc,
+                        repel = TRUE,            # Avoid label overlapping
+                        show.clust.cent = TRUE, # Show cluster centers
+                        palette = "jco",         # Color palette see ?ggpubr::ggpar
+                        ggtheme = theme_minimal(),
+                        main = "PCA - trees and shrubs")
+
+cluster
+
+# Let's look at the HCPC output
+
+# display the original data with a new column indicating which cluster they belong to
+
+head(res.hcpc$data.clust)
+
+# display the qualtitative variables that explain the most variance in each cluster
+
+res.hcpc$desc.var$quanti
+
+# principle dimensions that are most associated with clusters
+res.hcpc$desc.axes$quanti
+
+# save the output
+pdf("MFA_data_output/Cluster_analysis/PCA_glasshouse_climate_trees_shrubs.pdf") # Create a new pdf device
+print(var.plot)
+print(biplot)
+print(ind.plot)
+print(cluster)
+dev.off() # Close the pdf device
 
 
 
