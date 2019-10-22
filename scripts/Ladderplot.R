f2d9c09d73a7d90bf212804acaa3ee32c125ed5a
Sambam210 <samiya.tabassum@mq.edu.au>
2019-10-22 15:53:22 +1100

    created ladderplots with the LDMC clustering and also changed the concensus level from 60% to 75%

--- scripts/Ladderplot.R
+++ scripts/Ladderplot.R
@@ -313,6 +313,215 @@ pdf("Ladderplot_output/hortandtraits.pdf") # Create a new pdf device
 print(hortandtraitsplot)
 dev.off() # Close the pdf device
 
+#################################### HORT CLASSIFICATION VS TRAITS CLASSIFICATION W/ LDMC INSTEAD OF SUCCULENCE
+
+# Loading in my horticultural classification data
+
+hort.class <- read.csv("Ladderplot_data_input/gh_drought_summary_Samiya.csv")
+
+library(tidyverse)
+
+# get rid of source and line info
+
+hort.class <- hort.class[,1:7]
+
+# filter out the species with less than 4 sources
+
+hort.class <- filter(hort.class, total >= 4) # this gives us 92/113 species
+
+# create percentage columns for each of the columns 'no, 'moderate' and 'yes'
+
+hort.class <- hort.class %>%
+  mutate(No_percentage = No/total*100,
+         Moderate_percentage = Moderate/total*100,
+         Yes_percentage = Yes/total*100)
+
+# assign 'yes', 'no' or 'mixture' classifications 
+# Protocol: 
+# If >=60% of the records is either 'yes', 'no' or 'medium' then that species has that corresponding classification
+# If 'medium' is >=60% then that species is said to be 'yes' for drought tolerance
+# If no classification is >=60% then that species is 'mixture'
+
+hort.class <- hort.class %>%
+  mutate(hort_classification = case_when(No_percentage >= 60 ~ "drought intolerant",
+                                         Moderate_percentage >= 60 ~ "drought tolerant",
+                                         Yes_percentage >= 60 ~ "drought tolerant",
+                                         TRUE ~ "mixture")) %>%
+  select(Species_Code, hort_classification)
+
+# load my traits cluster data
+
+traits <- read.csv("PCA_Cluster_output/PCA_traits_cluster_output_LDMC.csv")
+
+traits <- traits %>%
+  rename(Species_Code=X) %>%
+  mutate(traits_classification = case_when(clust == "1" ~ "drought intolerant",
+                                           clust == "2" ~ "mixture",
+                                           clust == "3" ~ "drought tolerant")) %>%
+  select(Species_Code, traits_classification)
+
+# merge the two datasets together
+
+hortandtraits <- left_join(hort.class, traits, by = "Species_Code")
+
+# remove rows with NAs
+# https://stackoverflow.com/questions/26665319/removing-na-in-dplyr-pipe
+
+hortandtraits <- drop_na(hortandtraits) # ended up with 90 species 
+
+write.csv(hortandtraits, "Ladderplot_data_input/hortandtraits_LDMC.csv", row.names = FALSE)
+
+# let's look at some summaries of what has changed
+summary <- hortandtraits %>%
+  select(-Species_Code) %>%
+  group_by(hort_classification, traits_classification) %>%
+  add_count() %>%
+  distinct(hort_classification, traits_classification, .keep_all = TRUE)
+
+write.csv(summary, "Ladderplot_output/hortandtraits_LDMC_summary.csv", row.names = FALSE)
+
+#############################
+# Constructing the alluvial plot for hort and traits classification
+
+hortandtraits <- read.csv("Ladderplot_data_input/hortandtraits_LDMC.csv")
+
+#transform data into 'wide' format
+hortandtraits.wide <- gather(hortandtraits, key="method", value="classification", -Species_Code)
+
+# constructing the alluvial plot
+
+library(ggalluvial)
+library(ggplot2)
+
+hortandtraitsplot <- ggplot(hortandtraits.wide,
+                            aes(x = method, stratum = classification, alluvium = Species_Code,
+                                fill = classification, label = classification)) +
+  scale_x_discrete(expand = c(.1, .1)) +
+  geom_flow() +
+  geom_stratum(alpha = .5) +
+  geom_text(stat = "stratum", size = 3) +
+  annotate("text", x = 1, y = 82, label = "(12.2%)", size = 3) +
+  annotate("text", x = 1, y = 46, label = "(67.8%)", size = 3) +
+  annotate("text", x = 1, y = 6.8, label = "(20%)", size = 3) +
+  annotate("text", x = 2, y = 73, label = "(33.3%)", size = 3) +
+  annotate("text", x = 2, y = 45, label = "(27.8%)", size = 3) +
+  annotate("text", x = 2, y = 15, label = "(38.9%)", size = 3) +
+  theme(legend.position = "none") +
+  labs(y = "Number of species",
+       x = "Method")
+
+hortandtraitsplot
+
+# save the output
+pdf("Ladderplot_output/hortandtraits_LDMC.pdf") # Create a new pdf device
+print(hortandtraitsplot)
+dev.off() # Close the pdf device
+
+#######################################
+########## Let's try the same thing but with 75% concensus
+
+# Loading in my horticultural classification data
+
+hort.class <- read.csv("Ladderplot_data_input/gh_drought_summary_Samiya.csv")
+
+library(tidyverse)
+
+# get rid of source and line info
+
+hort.class <- hort.class[,1:7]
+
+# filter out the species with less than 4 sources
+
+hort.class <- filter(hort.class, total >= 4) # this gives us 92/113 species
+
+# create percentage columns for each of the columns 'no, 'moderate' and 'yes'
+
+hort.class <- hort.class %>%
+  mutate(No_percentage = No/total*100,
+         Moderate_percentage = Moderate/total*100,
+         Yes_percentage = Yes/total*100)
+
+# assign 'yes', 'no' or 'mixture' classifications 
+# Protocol: 
+# If >=75% of the records is either 'yes', 'no' or 'medium' then that species has that corresponding classification
+# If 'medium' is >=75% then that species is said to be 'yes' for drought tolerance
+# If no classification is >=75% then that species is 'mixture'
+
+hort.class <- hort.class %>%
+  mutate(hort_classification = case_when(No_percentage >= 75 ~ "drought intolerant",
+                                         Moderate_percentage >= 75 ~ "drought tolerant",
+                                         Yes_percentage >= 75 ~ "drought tolerant",
+                                         TRUE ~ "mixture")) %>%
+  select(Species_Code, hort_classification)
+
+# load my traits cluster data
+
+traits <- read.csv("PCA_Cluster_output/PCA_traits_cluster_output_LDMC.csv")
+
+traits <- traits %>%
+  rename(Species_Code=X) %>%
+  mutate(traits_classification = case_when(clust == "1" ~ "drought intolerant",
+                                           clust == "2" ~ "mixture",
+                                           clust == "3" ~ "drought tolerant")) %>%
+  select(Species_Code, traits_classification)
+
+# merge the two datasets together
+
+hortandtraits <- left_join(hort.class, traits, by = "Species_Code")
+
+# remove rows with NAs
+# https://stackoverflow.com/questions/26665319/removing-na-in-dplyr-pipe
+
+hortandtraits <- drop_na(hortandtraits) # ended up with 90 species 
+
+write.csv(hortandtraits, "Ladderplot_data_input/hortandtraits_LDMC_75percentconcensus.csv", row.names = FALSE)
+
+# let's look at some summaries of what has changed
+summary <- hortandtraits %>%
+  select(-Species_Code) %>%
+  group_by(hort_classification, traits_classification) %>%
+  add_count() %>%
+  distinct(hort_classification, traits_classification, .keep_all = TRUE)
+
+write.csv(summary, "Ladderplot_output/hortandtraits_LDMC_summary_75%concensus.csv", row.names = FALSE)
+
+#############################
+# Constructing the alluvial plot for hort and traits classification
+
+hortandtraits <- read.csv("Ladderplot_data_input/hortandtraits_LDMC_75%concensus.csv")
+
+#transform data into 'wide' format
+hortandtraits.wide <- gather(hortandtraits, key="method", value="classification", -Species_Code)
+
+# constructing the alluvial plot
+
+library(ggalluvial)
+library(ggplot2)
+
+hortandtraitsplot <- ggplot(hortandtraits.wide,
+                            aes(x = method, stratum = classification, alluvium = Species_Code,
+                                fill = classification, label = classification)) +
+  scale_x_discrete(expand = c(.1, .1)) +
+  geom_flow() +
+  geom_stratum(alpha = .5) +
+  geom_text(stat = "stratum", size = 3) +
+  annotate("text", x = 1, y = 84.8, label = "(7.8%)", size = 3) +
+  annotate("text", x = 1, y = 58, label = "(51.1%)", size = 3) +
+  annotate("text", x = 1, y = 16.2, label = "(41.1%)", size = 3) +
+  annotate("text", x = 2, y = 73, label = "(33.3%)", size = 3) +
+  annotate("text", x = 2, y = 45, label = "(27.8%)", size = 3) +
+  annotate("text", x = 2, y = 15, label = "(38.9%)", size = 3) +
+  theme(legend.position = "none") +
+  labs(y = "Number of species",
+       x = "Method")
+
+hortandtraitsplot
+
+# save the output
+pdf("Ladderplot_output/hortandtraits_LDMC_75percentconcensus.pdf") # Create a new pdf device
+print(hortandtraitsplot)
+dev.off() # Close the pdf device
+
 #####################################################################################################
 
 ############ HORT CLASSIFICATION VS TRAITSAI CLASSIFICATION
