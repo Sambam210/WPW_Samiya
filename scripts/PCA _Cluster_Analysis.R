ad3a98baac9a62fcabc2b371c5fef7762aedef43
Sambam210 <samiya.tabassum@mq.edu.au>
2019-10-23 14:29:56 +1100

    did PCA with leaf density but clusters are weird

--- scripts/PCA _Cluster_Analysis.R
+++ scripts/PCA _Cluster_Analysis.R
@@ -751,6 +751,168 @@ print(ind.plot)
 print(cluster)
 dev.off() # Close the pdf device
 
+#####################################
+### PCA on glasshouse variables 
+# LDMC instead of succulence
+# add leaf density (LMA/thickness) see Bartlett et al. 2012
+
+library(tidyverse)
+library("FactoMineR")
+library("factoextra")
+
+other.variables <- read.csv("PCA_Cluster_data/PCA_data.csv")
+
+other.variables <- filter(other.variables, Species_Code != "Crma", Species_Code != "Atfi",
+                          Species_Code != "Brru") # filter out species with missing leaf area
+
+other.variables <- select(other.variables, Species, Species_Code, mean_OsmPot, mean_LMA_gm2, Thickness_mm_Avg, mean_leaf_area_cm2)
+
+# load the LMA data
+LMA <- read.csv("GH_data/WPW_GH_LMA_clean.csv")
+
+# transform fresh and dry weights into LDMC
+LDMCandDensity <- LMA %>%
+  filter(Treatment == "C") %>%
+  select(Species, Species_Code, Fresh_Weight_g, Dry_Weight_g, LMA_gm2, Thickness_mm_Avg) %>%
+  mutate(LDMC = (Dry_Weight_g/Fresh_Weight_g),
+         Density = (LMA_gm2/Thickness_mm_Avg)) %>%
+  group_by(Species_Code) %>%
+  summarise(mean_LDMC = mean(LDMC, na.rm = TRUE),
+            mean_Density = mean(Density, na.rm = TRUE))
+
+# join to master spreadsheet
+PCA_analysis <- left_join(other.variables, LDMCandDensity, by = "Species_Code")
+
+## let's see if the data are linearly related
+
+# removed the categorical species columns
+
+PCA_analysis_pairs <- select(PCA_analysis, -Species, -Species_Code)
+
+# let's see if the data are linearly related
+
+pairs(PCA_analysis_pairs)
+
+# might need to transform the thickness and area variables
+
+PCA_analysis_pairs <- PCA_analysis_pairs %>%
+  mutate(log_Thickness_mm_Avg = log(Thickness_mm_Avg),
+         log_leaf_area_cm2 = log(mean_leaf_area_cm2)) %>%
+  select(-Thickness_mm_Avg, -mean_leaf_area_cm2)
+
+pairs(PCA_analysis_pairs) # looks better
+
+# transform in the main spreadsheet
+
+PCA_analysis <- PCA_analysis %>%
+  mutate(log_Thickness_mm_Avg = log(Thickness_mm_Avg),
+         log_leaf_area_cm2 = log(mean_leaf_area_cm2)) %>%
+  select(-Thickness_mm_Avg, -mean_leaf_area_cm2, -Species)
+
+# make species code the row name
+
+PCA_analysis <- PCA_analysis %>%
+  remove_rownames %>%
+  column_to_rownames(var="Species_Code")
+
+# doing the PCA
+
+res.pca <- PCA(PCA_analysis, graph = FALSE)
+
+# looking at the eigenvalues
+
+eig.val <- get_eigenvalue(res.pca)
+eig.val
+# first 3 PCs explain 87% of the data
+
+# let's look at the scree plot
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+
+# let's look at the variables
+
+var <- get_pca_var(res.pca)
+var
+
+# plot the variables
+
+var.plot <- fviz_pca_var(res.pca, col.var = "black")
+var.plot
+
+# colour variables on the correlation circle by their contrib value -> contribution of the variable on the factor map
+
+fviz_pca_var(res.pca, col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), # specify custom gradient
+             repel = TRUE) # avoid text overlap
+
+# Contributions of variables to PC1
+
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 4) # red dashed line is the expected average contribution (i.e. 4 variables = 25%)
+# LDMC and OsmPot and density
+
+# Contributions of variables to PC2
+
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 4)
+# Thickness and LMA
+
+# dimension description
+# identify the most significanlty associated variables for a given PC
+
+res.desc <- dimdesc(res.pca, axes = c(1,2), proba = 0.05)
+# Description of dimension 1
+res.desc$Dim.1 # LDMC and osmpot and density
+# Description of dimension 2
+res.desc$Dim.2 # thickness and LMA
+
+# graph of individuals
+
+ind <- get_pca_ind(res.pca)
+ind
+
+# plot the individuals
+
+ind.plot <- fviz_pca_ind(res.pca, repel = TRUE)
+ind.plot
+
+# colour by groups
+
+# confidence ellipses
+# shows 95% confidence ellipse, see ?coord.ellipse()
+
+fviz_pca_ind(res.pca,
+             geom.ind = "point", # show points only (but not "text")
+             col.ind = PCA_analysis$Growth_Form, # color by growth form
+             palette = c("Dark2"),
+             addEllipses = TRUE, ellipse.type = "confidence", # confidence ellipses
+             legend.title = "Growth form") # grasses look sign different from everything else
+
+# Hierarchical clustering
+
+# let's only do the PCA on the first 3 dimensions
+
+res.pca <- PCA(PCA_analysis, ncp = 3, graph = FALSE)
+
+# clustering
+res.hcpc <- HCPC(res.pca, graph = FALSE)
+
+# dendrogram
+fviz_dend(res.hcpc, 
+          cex = 0.7,                     # Label size
+          palette = "jco",               # Color palette see ?ggpubr::ggpar
+          rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
+          rect_border = "jco",           # Rectangle color
+          labels_track_height = 0.8)     # Augment the room for labels
+
+# graph
+cluster <- fviz_cluster(res.hcpc,
+                        repel = TRUE,            # Avoid label overlapping
+                        show.clust.cent = TRUE, # Show cluster centers
+                        palette = "jco",         # Color palette see ?ggpubr::ggpar
+                        ggtheme = theme_minimal(),
+                        main = "PCA")
+cluster
+
+# SCRATCH, CLUSTERS ARE WEIRD
 
 ######################################################################################################################################
 ################################################## PCA on glasshouse variables and aridity ###############################################################
