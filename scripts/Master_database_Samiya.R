d3354d8f8bf6b6682941d6a07160b28c79c350c3
Sambam210 <samiya.tabassum@mq.edu.au>
2020-02-11 15:27:59 +1100

    played around a bit

--- scripts/Master_database_Samiya.R
+++ scripts/Master_database_Samiya.R
@@ -10,3 +10,23 @@ test <- Master_Key %>%
 
 write.csv(test,"Master_database_output/genus_cultivar.csv",row.names=FALSE)
 
+########################################################################################################################################
+
+## pick out the species from the master key that were grown in the glasshouse and have a model
+
+library(tidyverse)
+
+whatwehave <- Master_Key %>%
+  filter(!is.na(gl) | !is.na(mod))
+# 2121 species
+
+# list of glasshouse species and their scientific names
+
+glasshouse <- whatwehave %>%
+  filter(!is.na(gl)) # some species have multiple entries as we tested multiple varieties
+
+write.csv(glasshouse, "Master_database_output/glasshouse_list.csv", row.names = FALSE)
+
+glasshouse <- whatwehave %>%
+  filter(!is.na(gl)) %>%
+  distinct(scientificNameStd, .keep_all = TRUE) # add correa, cynodon and plantanus. In traits database, Correa is Correa pulchella, cynodon is Cynodon dactylon and plantanus is Plantanus acerifolia. These are not recognised scientific names.
