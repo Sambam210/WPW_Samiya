b9b7046b46b862a66b086e626e08e47f6314f3ac
Samiya Tabassum <samiya.tabassum@mq.edu.au>
2020-04-28 17:20:35 +1000

    fixed acsm LMA average

--- scripts/My_analysis.R
+++ scripts/My_analysis.R
@@ -127,6 +127,11 @@ ggplot(LMA, aes(x = Batch, y = mean_succulance_g)) +
 
 LMA <- read.csv("GH_data/WPW_GH_LMA_clean.csv") # Renee uploaded an updated version of the LMA data with errors fixed
 
+# remove ACSM plants that have wrong leaf areas and therefore LMA
+LMA <- LMA %>%
+  filter(Sample_ID != "Acsm6C", Sample_ID != "Acsm8C", Sample_ID != "Acsm9C", 
+         Sample_ID != "Acsm10C")
+
 LMA <- LMA %>%
   filter(Treatment == 'C') %>% # filter for only the controls
   select(Species_Code, Fresh_Weight_g, Dry_Weight_g, Thickness_mm_Avg, LMA_gm2) %>% # select relevant columns
